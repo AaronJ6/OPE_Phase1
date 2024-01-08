1eba6688056498767ccd268b34375e3f528da064
AmalkrishnaAS <amalkrishna02016@gmail.com>
2024-01-08 13:56:54 +0530

    tokenizer test

--- test.py
+++ test.py
@@ -1,13 +1,40 @@
-#tokenize sql using sql glot
+#tokenizer from sqlglot
+from sqlglot import Tokenizer,TokenType
+#pretty print
+from pprint import pprint
 
-from sqlglot import parse
-import json
-import sys
+#tokenizer from sqlparse
 
-def main():
-    query = sys.argv[1]
-    parsed = parse(query)
-    print(parsed)
-    parsed.pop('type', None)
-if __name__ == "__main__":
-    main()
+tokenizer = Tokenizer()
+tokens = tokenizer.tokenize("SELECT * FROM foo WHERE bar = '1' AND baz = 2")
+
+
+# for token in tokens:
+#     print(token.text, token.token_type)
+    
+
+#seperate numbers and strings
+strings = []
+numbers = []
+variables = []
+for token in tokens:
+    if token.token_type == TokenType.STRING:
+        strings.append({
+            'text': token.text,
+            'type': 'string'
+        })
+    elif token.token_type == TokenType.NUMBER:
+        numbers.append({
+            'text': token.text,
+            'type': 'number'
+        })
+    elif token.token_type == TokenType.VAR:
+        variables.append({
+            'text': token.text,
+            'type': 'variable'
+        })
+pprint(strings)
+pprint(numbers)
+pprint(variables)
+        
+        
